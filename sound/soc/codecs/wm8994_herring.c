96e9caae75649a7e9e22d23b2d6a6ec1404d2683
Suryandaru Triandana <syndtr@gmail.com>
2011-10-28 16:43:40 +0700

    sound: little fix for atlas

--- sound/soc/codecs/wm8994_herring.c
+++ sound/soc/codecs/wm8994_herring.c
@@ -2587,7 +2587,7 @@ static void wm8994_set_gsm_voicecall_common_setting(struct snd_soc_codec *codec)
 
 void wm8994_set_voicecall_common_setting(struct snd_soc_codec *codec)
 {
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_set_cdma_voicecall_common_setting(codec);
 	else
 		wm8994_set_gsm_voicecall_common_setting(codec);
@@ -2810,7 +2810,7 @@ static void wm8994_set_gsm_voicecall_receiver(struct snd_soc_codec *codec)
 
 void wm8994_set_voicecall_receiver(struct snd_soc_codec *codec)
 {
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_set_cdma_voicecall_receiver(codec);
 	else
 		wm8994_set_gsm_voicecall_receiver(codec);
@@ -2931,7 +2931,7 @@ void wm8994_set_voicecall_headset(struct snd_soc_codec *codec)
 
 	wm8994_write(codec, WM8994_POWER_MANAGEMENT_3, 0x0030);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3088,7 +3088,7 @@ void wm8994_set_voicecall_headphone(struct snd_soc_codec *codec)
 
 	wm8994_write(codec, WM8994_POWER_MANAGEMENT_3, 0x0030);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3177,7 +3177,7 @@ void wm8994_set_voicecall_speaker(struct snd_soc_codec *codec)
 	wm8994_write(codec, WM8994_POWER_MANAGEMENT_4,
 			WM8994_AIF2ADCL_ENA | WM8994_ADCL_ENA);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3275,7 +3275,7 @@ void wm8994_set_voicecall_bluetooth(struct snd_soc_codec *codec)
 	wm8994_write(codec, WM8994_DAC2_RIGHT_MIXER_ROUTING,
 		WM8994_AIF2DACR_TO_DAC2R | WM8994_AIF1DAC1R_TO_DAC2R);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3410,7 +3410,7 @@ void wm8994_set_voicecall_tty_vco(struct snd_soc_codec *codec)
 
 	wm8994_write(codec, WM8994_POWER_MANAGEMENT_3, 0x0030);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3576,7 +3576,7 @@ void wm8994_set_voicecall_tty_hco(struct snd_soc_codec *codec)
 		WM8994_MIXOUTLVOL_ENA | WM8994_MIXOUTRVOL_ENA |
 		WM8994_MIXOUTL_ENA | WM8994_MIXOUTR_ENA);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3740,7 +3740,7 @@ void wm8994_set_voicecall_tty_full(struct snd_soc_codec *codec)
 
 	wm8994_write(codec, WM8994_POWER_MANAGEMENT_3, 0x0030);
 
-	if (herring_is_cdma_wimax_dev())
+	if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0009);
 	else
 		wm8994_write(codec, WM8994_AIF2_CLOCKING_1, 0x0019);
@@ -3801,7 +3801,7 @@ int wm8994_set_codec_gain(struct snd_soc_codec *codec, u16 mode, u16 device)
 
 	if (mode == PLAYBACK_MODE) {
 
-		if (herring_is_cdma_wimax_dev())
+		if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 			default_gain_table_p = cdma_playback_gain_table;
 		else
 			default_gain_table_p = playback_gain_table;
@@ -3842,7 +3842,7 @@ int wm8994_set_codec_gain(struct snd_soc_codec *codec, u16 mode, u16 device)
 			break;
 		}
 	} else if (mode == VOICECALL_MODE) {
-		if (herring_is_cdma_wimax_dev())
+		if (machine_is_atlas() || herring_is_cdma_wimax_dev())
 			default_gain_table_p = cdma_voicecall_gain_table;
 		else
 			default_gain_table_p = voicecall_gain_table;
